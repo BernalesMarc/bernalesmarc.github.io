8a86895357661baa6436077e738aba7596f31608
mBernales659 <102293703+mBernales659@users.noreply.github.com>
2024-01-28 21:59:27 +0800

    projects section update
    hindi na gumagalaw ang contact section pag nag hover sa projects
    
    need lang pala ifixed ang height

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -2,47 +2,47 @@ ScrollReveal({
             reset: false,
             distance: '20px',
             duration: 2500,
-            delay: 400
+            delay: 250
         });
 
         ScrollReveal().reveal('.header-text, .sub-title, .about-col-2', {
-            delay: 300,
+            delay: 150,
             origin: 'left'
         });
         ScrollReveal().reveal('.contact-left p', {
-            delay: 300,
+            delay: 150,
             origin: 'left',
             interval: 200
         });
         ScrollReveal().reveal('.social-icons i, nav img', {
-            delay: 350,
+            delay: 250,
             origin: 'top',
             interval: 200
         });
         ScrollReveal().reveal('.about-col-1 img', {
-            delay: 400,
+            delay: 250,
             origin: 'right'
         });
         ScrollReveal().reveal('.about-col-2 p, .tab-titles', {
-            delay: 400,
+            delay: 250,
             origin: 'top'
         });
         ScrollReveal().reveal('.expertise-list,.expertise-list div', {
-            delay: 400,
+            delay: 250,
             origin: 'bottom',
             interval: 200
         });
         ScrollReveal().reveal('.certificate-list, .certificate-list .certificate img, .project-list div', {
-            delay: 500,
+            delay: 250,
             origin: 'bottom',
             interval: 200
         });
         ScrollReveal().reveal('.btn,.accordion',{
-            delay: 400,
+            delay: 300,
             origin: 'top'
         });
         ScrollReveal().reveal('.contact-right form input, textarea', {
-            delay: 500,
+            delay: 200,
             origin: 'left',
             interval: 200
         });
@@ -50,11 +50,11 @@ ScrollReveal({
         ScrollReveal().reveal('.technologies-container_main-contents .images img', {
             origin: 'bottom',
             interval: 200,
-            delay: 300 // Add a delay of 200ms for each image
+            delay: 150 // Add a delay of 200ms for each image
           });
 
           ScrollReveal().reveal('.technologies-container_main-contents', {
             origin: 'top',
-            interval: 400,
-            delay: 250 // Add a delay of 200ms for each ".col" element inside ".technologies-container"
+            interval: 100,
+            delay: 150 // Add a delay of 200ms for each ".col" element inside ".technologies-container"
           });
