f6a4474d6c44a700ac72bd70bbdaf223e0bd2e62
mBernales659 <102293703+mBernales659@users.noreply.github.com>
2024-01-28 22:12:42 +0800

    scroll reveal modfied
    pinabilis pa ang pag lolload ng animation

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -6,51 +6,51 @@ ScrollReveal({
         });
 
         ScrollReveal().reveal('.header-text, .sub-title, .about-col-2', {
-            delay: 150,
+            delay: 100,
             origin: 'left'
         });
         ScrollReveal().reveal('.contact-left p', {
-            delay: 150,
+            delay: 100,
             origin: 'left',
-            interval: 200
+            interval: 150
         });
         ScrollReveal().reveal('.social-icons i, nav img', {
-            delay: 250,
+            delay: 200,
             origin: 'top',
-            interval: 200
+            interval: 150
         });
         ScrollReveal().reveal('.about-col-1 img', {
-            delay: 250,
+            delay: 200,
             origin: 'right'
         });
         ScrollReveal().reveal('.about-col-2 p, .tab-titles', {
-            delay: 250,
+            delay: 200,
             origin: 'top'
         });
         ScrollReveal().reveal('.expertise-list,.expertise-list div', {
-            delay: 250,
+            delay: 200,
             origin: 'bottom',
-            interval: 200
+            interval: 150
         });
         ScrollReveal().reveal('.certificate-list, .certificate-list .certificate img, .project-list div', {
-            delay: 250,
+            delay: 100,
             origin: 'bottom',
-            interval: 200
+            interval: 100
         });
         ScrollReveal().reveal('.btn,.accordion',{
-            delay: 300,
+            delay: 200,
             origin: 'top'
         });
         ScrollReveal().reveal('.contact-right form input, textarea', {
-            delay: 200,
+            delay: 100,
             origin: 'left',
-            interval: 200
+            interval: 100
         });
 
         ScrollReveal().reveal('.technologies-container_main-contents .images img', {
             origin: 'bottom',
-            interval: 200,
-            delay: 150 // Add a delay of 200ms for each image
+            interval: 100,
+            delay: 120 // Add a delay of 200ms for each image
           });
 
           ScrollReveal().reveal('.technologies-container_main-contents', {
