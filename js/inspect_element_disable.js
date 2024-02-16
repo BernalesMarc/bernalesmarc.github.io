d5e9967fed0e3e85b5cd818a029b85b66a28a9c9
mBernales659 <102293703+mBernales659@users.noreply.github.com>
2024-02-16 11:18:24 +0800

    Glamorous logo has been added and ctrl has been disabled

--- js/inspect_element_disable.js
+++ js/inspect_element_disable.js
@@ -10,4 +10,12 @@ document.onkeydown = function(e) {
     }
 };
 
+document.addEventListener("keydown", function (e) {
+    // Check if the pressed key is 'S' and the Ctrl key is pressed
+    if ((e.key === 's' || e.key === 'S') && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault(); // Prevent the default behavior (saving)
+        console.log("Ctrl+S is disabled");
+    }
+});
+
 document.body.style.userSelect = 'none';
